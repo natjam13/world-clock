9d6b2a94e91f681bbaf8a8f01f80ef1ecdd98f4d
natjam13 <murrayn2@sky.com>
2025-03-09 18:23:48 +0000

    created functions in JS and search for better  UX

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-let londonElement = document.querySelector("#london");
-let londonDateElement = londonElement.querySelector(".date");
-let londonTimeElement = londonElement.querySelector(".time"); 
-let londonTime = moment().tz("Europe/London");
+function updateTime() {
 
-londonDateElement.innerHTML = londonTime.format(`MMMM Do YYYY`);
-londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+    let londonElement = document.querySelector("#london");
+    if (londonElement) {
+        let londonDateElement = londonElement.querySelector(".date");
+        let londonTimeElement = londonElement.querySelector(".time");
+        let londonTime = moment().tz("Europe/London");
 
+        londonDateElement.innerHTML = londonTime.format(`MMMM Do YYYY`);
+        londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 
-let newYorkElement = document.querySelector("#new-york");
-let newYorkDateElement = newYorkElement.querySelector(".date");
-let newYorkTimeElement = newYorkElement.querySelector(".time"); 
-let newYorkTime = moment().tz("America/New_York");
+    let newYorkElement = document.querySelector("#new-york");
+    if (newYorkElement) {
+        let newYorkDateElement = newYorkElement.querySelector(".date");
+        let newYorkTimeElement = newYorkElement.querySelector(".time");
+        let newYorkTime = moment().tz("America/New_York");
 
-newYorkDateElement.innerHTML = newYorkTime.format(`MMMM Do YYYY`);
-newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss [<small>]A[</small>]");
+        newYorkDateElement.innerHTML = newYorkTime.format(`MMMM Do YYYY`);
+        newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss [<small>]A[</small>]");
+    }
+}
 
+function updateCity(event) {
 
-let sydneyElement = document.querySelector("#sydney");
-let sydneyDateElement = sydneyElement.querySelector(".date");
-let sydneyTimeElement = sydneyElement.querySelector(".time"); 
-let sydneyTime = moment().tz("Austrailia/Sydney");
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+     <div class="city">
+            <div>
+            <h2>${cityTimeZone}</h2>
+            <div class="date">${cityTime.format(`MMMM Do YYYY`)}</div>
+            </div>
+            <div class="time">${cityTime.format("h:mm:ss ")}<small>${cityTime.format("A")}</small></div>
+         </div>
+    `;
+}
 
-sydneyDateElement.innerHTML = sydneyTime.format(`MMMM Do YYYY`);
-sydneyIimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
 
 
 
